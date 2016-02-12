cf6c12c93673577726f3e385db53a8b73943059e
Oleg Burov <oleg.burov@outlook.com>
2016-02-11 21:54:26 -0800

    Implemented Sitecore Patch for issue #96688.

---
+++ src/Sitecore.Support.96688/Data/DataUri.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.Serialization;
+using Sitecore.Data;
+using Sitecore.Globalization;
+
+namespace Sitecore.Support.Data
+{
+  class DataUri : Sitecore.Data.DataUri
+  {
+    #region Constructors
+
+    public DataUri(Sitecore.Data.DataUri uri) : base(uri)
+    {
+    }
+
+    public DataUri(ItemUri uri) : base(uri)
+    {
+    }
+
+    public DataUri(string dataUri) : base(dataUri)
+    {
+    }
+
+    public DataUri(ID itemID) : base(itemID)
+    {
+    }
+
+    public DataUri(ID itemID, Language language) : base(itemID, language)
+    {
+    }
+
+    public DataUri(ID itemID, Language language, Version version) : base(itemID, language, version)
+    {
+    }
+
+    public DataUri(string path, Language language) : base(path, language)
+    {
+    }
+
+    public DataUri(string path, Language language, Version version) : base(path, language, version)
+    {
+    }
+
+    public DataUri(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    #endregion
+
+    public override bool Equals(object obj)
+    {
+      if (obj == null)
+      {
+        return false;
+      }
+
+      DataUri uri = obj as DataUri;
+
+      if (uri == null)
+      {
+        return false;
+      }
+
+      return this.ItemID == uri.ItemID && this.Version == uri.Version && this.Language == uri.Language && (this.ItemID != (ID)null || Path == uri.Path);
+    }
+
+  }
+}
---
+++ src/Sitecore.Support.96688/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.96688")]
+[assembly: AssemblyProduct("Sitecore.Support.96688")]
+[assembly: AssemblyVersion("8.1.0.0")]
+[assembly: AssemblyFileVersion("8.1.0.0")]
+[assembly: AssemblyInformationalVersion("8.1.0 rev. 160128")]
+
+[assembly: AssemblyDescription("Sitecore Support Patch")]
+[assembly: AssemblyCompany("Sitecore Corporation")]
+[assembly: AssemblyCopyright("Copyright © 2016 Sitecore Corporation")]
+[assembly: AssemblyTrademark("Sitecore® is a registered trademark of Sitecore Corporation")]
+[assembly: ComVisible(false)]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("007c5b2f-8945-41a8-a01f-cef47662d8f0")]
